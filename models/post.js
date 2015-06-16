59085365517511827747f52464817ff7b64c8d8e
Raincal <cyj94228@gmail.com>
2015-06-17 03:37:59 +0800

    add markdown

--- models/post.js
+++ models/post.js
@@ -2,6 +2,7 @@
  * Created by Yujia on 2015/6/17.
  */
 var mongodb = require('./db');
+var markdown = require('markdown').markdown;
 
 function Post(name, title, post) {
     this.name = name;
@@ -80,6 +81,11 @@ Post.get = function(name, callback) {
                 if (err) {
                     return callback(err);//失败！返回 err
                 }
+                //解析 markdown 为 html
+                docs.forEach(function (doc) {
+                    doc.post = markdown.toHTML(doc.post);
+                });
+
                 callback(null, docs);//成功！以数组形式返回查询的结果
             });
         });
